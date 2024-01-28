ec67027db2e537680424b86ecd517af30a370a65
Dhyana Iris <dhyanairis@gmail.com>
2024-01-28 18:58:14 -0300

    criando pagina content

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": 1,
+    "photo": "aaa",
+    "title": "saiu novo filme",
+    "description": "ola"
+  },
+  {
+    "id": 2,
+    "photo": "BBB",
+    "title": "saiu novo filme 2",
+    "description": "olaAAAA"
+  }
+]
---
+++ src/app/pages/content/content.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-content',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './content.component.html',
+  styleUrl: './content.component.css'
+})
+export class ContentComponent {
+
+  photoCover: string = ''
+  contentTitle: string = ''
+  contentDescription: string = ''
+
+  constructor() {}
+
+}
